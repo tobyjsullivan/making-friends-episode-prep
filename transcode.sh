9f65203c4569ee52c1560090343b95b0ac95af98
Toby Sullivan <tobyjsullivan@gmail.com>
2014-02-22 19:59:33 -0800

    Added proper handling of filenames with spaces

--- transcode.sh
+++ transcode.sh
@@ -4,17 +4,20 @@ IN=$1
 OUT=$2
 ART=album_art.jpg
 
+echo "Input file: " $IN
+echo "Output file: " $OUT
+
 # Transcode
-ffmpeg -i $IN -c:a libfaac -vn -b:a 64k $OUT
+ffmpeg -i "$IN" -c:a libfaac -vn -b:a 64k "$OUT"
 
 echo "Done transcoding"
 echo "Adding album art..."
 
 # Add album art
-./add-album-art $OUT $ART
+./add-album-art "$OUT" "$ART"
 
 echo "Done adding album art"
 echo "Track length:"
 
 # Print length
-./audio-length $OUT
+./audio-length "$OUT"
